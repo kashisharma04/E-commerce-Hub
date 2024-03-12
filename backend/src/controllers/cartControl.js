a3f5c7f1f15df7b2509f30c03961bd11386c12b9
GauravPande123webdeveloper <gauravpandeyidforfunctionup@gmail.com>
2024-03-12 22:33:49 +0530

    changes in cart

--- backend/src/controllers/cartControl.js
+++ backend/src/controllers/cartControl.js
@@ -4,12 +4,13 @@ const cart = require('../models/cart');
 
 const createCart = async (req,res)=>{
     let data = req.body;
+    console.log(data);
     const result = await cart.create(data);
     return res.status(201).send({Status : true, msg : "Product Created Successfully" , result : result})
 }
 const getCart = async function(req,res){
     let data = await cart.find()
-    console.log(data);
+    // console.log(data);
     return res.status(201).send({status : true , msg : data })
     
 }
